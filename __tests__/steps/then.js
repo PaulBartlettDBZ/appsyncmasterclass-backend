ece5343b5d295bff3973e01aaa3d394a60f6b41a
PaulBartlett <Paul.Bartlett@debenhams.com>
2022-08-09 14:29:01 +0100

    implement tweet (and fix various random typos)

--- __tests__/steps/then.js
+++ __tests__/steps/then.js
@@ -39,8 +39,62 @@ const user_can_download_image_from = async (url) => {
     return resp.data
 }
 
+const tweet_exists_in_TweetsTable = async (id) => {
+    const DynamoDB = new AWS.DynamoDB.DocumentClient()
+
+    console.log(`looking for tweet [${id}] in [${process.env.TWEETS_TABLE}]`)
+
+    const resp = await DynamoDB.get({
+        TableName: process.env.TWEETS_TABLE,
+        Key: {
+            id
+        }
+    }).promise()
+
+    expect(resp.Item).toBeTruthy()
+    return resp.Item
+}
+const tweet_exists_in_TimelinesTable = async (userId, tweetId) => {
+    const DynamoDB = new AWS.DynamoDB.DocumentClient()
+
+    console.log(`looking for tweet [${tweetId}] in [${process.env.TIMELINES_TABLE}]`)
+
+    const resp = await DynamoDB.get({
+        TableName: process.env.TIMELINES_TABLE,
+        Key: {
+            userId,
+            tweetId
+        }
+    }).promise()
+
+    expect(resp.Item).toBeTruthy()
+    return resp.Item
+}
+const tweetsCount_is_updated_in_usersTable = async (id, newCount) => {
+    const DynamoDB = new AWS.DynamoDB.DocumentClient()
+
+    console.log(`looking for user [${id}] in [${process.env.USERS_TABLE}]`)
+
+    const resp = await DynamoDB.get({
+        TableName: process.env.USERS_TABLE,
+        Key: {
+            id
+        }
+    }).promise()
+    console.log(resp)
+
+    expect(resp.Item).toBeTruthy()
+    expect(resp.Item.tweetsCount).toEqual(newCount)
+
+    return resp.Item
+}
+
 module.exports = {
     user_exists_in_UsersTable,
     user_can_upload_image_to_url,
-    user_can_download_image_from
+    user_can_download_image_from,
+    tweet_exists_in_TweetsTable,
+    tweet_exists_in_TimelinesTable,
+    tweetsCount_is_updated_in_usersTable
+
 }
--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -161,6 +161,41 @@ const a_user_calls_getImageUploadUrl = async (user, extension, contentType) => {
       return url
 }
 
+const we_invoke_tweet = async (username, text) => {
+    const handler = require('../../functions/tweet').handler
+    const context = {}
+    const event = {
+      identity: {
+        username
+      },
+      arguments: {
+        text
+      }
+    }
+    return  await handler(event, context)
+
+}
+
+const a_user_calls_tweet = async (user, text) => {
+    const tweet = `mutation tweet($text: String!) {
+        tweet(text: $text) {
+            id
+            createdAt
+            replies
+            text
+            likes
+            retweets
+        }
+      }`
+
+      const variables = {
+        text: text
+      }
+      const data = await GraphQL(process.env.API_URL, tweet, variables, user.accessToken)
+      const newTweet = data.tweet
+      console.log(`[${user.username}] - tweeted`)
+      return newTweet
+}
 
 module.exports = {
     we_invoke_confirmUserSignup,
@@ -169,6 +204,8 @@ module.exports = {
     a_user_calls_getMyProfile,
     a_user_calls_editMyProfile,
     we_invoke_getImageUploadUrl,
-    a_user_calls_getImageUploadUrl
+    a_user_calls_getImageUploadUrl,
+    we_invoke_tweet,
+    a_user_calls_tweet
 
 }
